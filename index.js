ea6530368f4b2eaeefc8a7a1b52c1cbad44f9c33
rufetisr <rufet.isr123@gmail.com>
2023-07-30 20:22:13 +0400

    project add

---
+++ index.js
@@ -0,0 +1,26 @@
+let inp = document.querySelector('input');
+let img = document.querySelector('img');
+let btn = document.querySelector('button');
+
+inp.addEventListener('change', (e) => {
+    let locImg = URL.createObjectURL(e.target.files[0]);
+    img.src = locImg;
+})
+
+btn.addEventListener('click', () => {
+    var qr = new QrcodeDecoder();
+    document.querySelector('span').innerHTML = "";
+    document.querySelector('p').innerHTML = "";
+    
+    qr.decodeFromImage(img).then((res) => {
+        console.log(res);
+        if (!res.data.includes(';')) {
+            document.querySelector('span').innerHTML = res.data;            
+        }
+        document.querySelector('p').innerHTML = res.data.split(";")[2].slice(2);
+    });
+})
+
+document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault();
+})
